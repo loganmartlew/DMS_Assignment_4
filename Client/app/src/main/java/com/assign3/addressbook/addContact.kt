5e92b5016cdbdad2aa22968d6aa60b376e2edf41
Coen <48755627+Nyxxi@users.noreply.github.com>
2022-06-05 10:55:37 +1200

    changed some stuff
    made a contacts view window pretty basic right now and am working on the contact item for the list atm

---
+++ Client/app/src/main/java/com/assign3/addressbook/addContact.kt
@@ -0,0 +1,11 @@
+package com.assign3.addressbook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class addContact : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_contact)
+    }
+}
