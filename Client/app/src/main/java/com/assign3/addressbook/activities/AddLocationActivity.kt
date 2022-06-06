372341b391702905391a46222153225da71e6590
Coen <48755627+Nyxxi@users.noreply.github.com>
2022-06-06 21:14:29 +1200

    added so the login name is always available

--- Client/app/src/main/java/com/assign3/addressbook/activities/AddLocationActivity.kt
+++ Client/app/src/main/java/com/assign3/addressbook/activities/AddLocationActivity.kt
@@ -1,12 +1,26 @@
 package com.assign3.addressbook.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.assign3.addressbook.R
 
 class AddLocationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_contact)
+
+        val bundle: Bundle? = intent.extras;
+        val name: String? = intent.getStringExtra("Username");
+
+        val intent = Intent(this@AddLocationActivity, LocationsListActivity::class.java);
+
+        val button: Button = findViewById(R.id.addContactButton);
+
+        button.setOnClickListener{
+            intent.putExtra("Username", name);
+            startActivity(intent);
+        }
     }
 }
--- Client/app/src/main/java/com/assign3/addressbook/activities/LocationsListActivity.kt
+++ Client/app/src/main/java/com/assign3/addressbook/activities/LocationsListActivity.kt
@@ -11,10 +11,14 @@ class LocationsListActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contacts_page)
 
+        val bundle: Bundle? = intent.extras;
+        val name: String? = intent.getStringExtra("Username");
+
         val addContactButton: View = findViewById(R.id.addContactButton);
         val intent = Intent(this@LocationsListActivity, AddLocationActivity::class.java);
 
         addContactButton.setOnClickListener{
+            intent.putExtra("Username", name);
             startActivity(intent);
         }
     }
