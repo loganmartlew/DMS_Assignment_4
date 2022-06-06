e3ea08de8bdb243554354cdba47dec807252d438
Logan Martlew <logan.martlew@gmail.com>
2022-06-06 20:19:55 +1200

    Added FAB to locations page

--- Client/app/src/main/java/com/assign3/addressbook/MainActivity.kt
+++ Client/app/src/main/java/com/assign3/addressbook/MainActivity.kt
@@ -9,12 +9,11 @@ import android.widget.Button
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-    val username = null;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var buttonLogin: Button = findViewById(R.id.LoginButton);
+        val buttonLogin: Button = findViewById(R.id.LoginButton);
         val intent = Intent(this@MainActivity, ContactsPage::class.java);
         var username: EditText = findViewById(R.id.Nameet);
         var uName = username.text.toString()
