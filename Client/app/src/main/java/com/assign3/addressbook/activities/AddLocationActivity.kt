ba0e7d60415006e2794adf4ae1fd7350335aed8a
Coen <48755627+Nyxxi@users.noreply.github.com>
2022-06-06 23:57:35 +1200

    Map and location stuff

--- Client/app/src/main/java/com/assign3/addressbook/activities/AddLocationActivity.kt
+++ Client/app/src/main/java/com/assign3/addressbook/activities/AddLocationActivity.kt
@@ -1,27 +1,54 @@
 package com.assign3.addressbook.activities
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.location.Address
+import android.location.Geocoder
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 import com.assign3.addressbook.R
+import java.util.*
+
 
 class AddLocationActivity : AppCompatActivity() {
+
+    var lat = 0.0
+    var lng = 0.0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_add_location)
 
-        val bundle: Bundle? = intent.extras;
         val name: String? = intent.getStringExtra("Username");
 
-        val intent = Intent(this@AddLocationActivity, LocationsListActivity::class.java);
+        val address: EditText = findViewById(R.id.nameEditText);
 
+        getLatLongFromAddress(address.text.toString());
+
+        val intent = Intent(this@AddLocationActivity, LocationsListActivity::class.java);
         val button: Button = findViewById(R.id.addContactButton);
 
         button.setOnClickListener{
             intent.putExtra("Username", name);
             startActivity(intent);
+        }
+    }
+
+    private fun getLatLongFromAddress(address: String) {
+        val geoCoder = Geocoder(this, Locale.getDefault())
+        try {
+            val addresses: List<Address> = geoCoder.getFromLocationName(address, 1)
+            if (addresses.isNotEmpty()) {
+                lat = addresses[0].getLatitude() as Double;
+                lng = addresses[0].getLongitude() as Double;
+                Log.d("Latitude", "" + lat)
+                Log.d("Longitude", "" + lng)
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
     }
 }
--- Client/app/src/main/java/com/assign3/addressbook/activities/LocationDetailsActivity.kt
+++ Client/app/src/main/java/com/assign3/addressbook/activities/LocationDetailsActivity.kt
@@ -2,11 +2,50 @@ package com.assign3.addressbook.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.assign3.addressbook.R
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.model.MarkerOptions
 
-class LocationDetailsActivity : AppCompatActivity() {
+class LocationDetailsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    val lat get() = intent.getDoubleExtra("lat", 0.0)
+    val long get() = intent.getDoubleExtra("long", 0.0)
+    val name get() = intent.getStringExtra("name")
+    val address get() = intent.getStringExtra("address")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_location_details)
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.mapView) as? SupportMapFragment
+        mapFragment?.getMapAsync(this)
+
+        val mapOwnerTextView = findViewById<TextView>(R.id.PersonNametv).apply {
+            text = name
+        }
+        val addressTextView = findViewById<TextView>(R.id.addressstv).apply {
+            text = address
+        }
+        val latTextView = findViewById<TextView>(R.id.latitudetv).apply {
+            text = lat.toString()
+        }
+        val longtTextView = findViewById<TextView>(R.id.longitudetv).apply {
+            text = long.toString()
+        }
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        val addressMarker = LatLng(lat, long)
+        googleMap.addMarker(
+            MarkerOptions()
+                .position(addressMarker)
+                .title(name + "'s Location")
+        )
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(addressMarker))
     }
 }
